fcaeb62659542352ca6d306fc0ae56b3684f3291
Ubuntu <ubuntu@ip-172-31-79-182.ec2.internal>
2022-04-06 13:30:54 +0000

    program asks the user for an integer then tells them if they are right

--- num_guess_try_catch.py
+++ num_guess_try_catch.py
@@ -2,7 +2,8 @@
 # Created By: Titwech Wal
 # Date: April.5. 2022
 
-# ...
+# program asks the user for an integer
+# then tells them if they're right
 
 from colorama import Fore
 import random
@@ -11,29 +12,28 @@ import random
 def main():
     # a number between 1 and 100
     random_variable = random.randint(1, 10)
- 
-    
-    # try catch 
+
+    # try catch
     try:
-        # string into integer 
+        # string into integer
         user_guess = input("Enter a integer between (0-10): ")
         user_guess_as_int = int(user_guess)
-        
+
         # check if number is correct or incorrect
         if user_guess == random_variable:
             print(Fore.GREEN + "You guessed right")
-    
+
         else:
             print(Fore.RED + "You guessed wrong the integer is: {}"
-              .format(random_variable))
+                  .format(random_variable))
             print("")
-             
+
     except Exception:
         print("That is not a integer")
         print("")
-    
-    finally: 
-        print(Fore.YELLOW +"Thanks for playing!")
+
+    finally:
+        print(Fore.YELLOW + "Thanks for playing!")
 
 
 if __name__ == "__main__":
